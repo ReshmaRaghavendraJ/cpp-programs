6068058358bf7f885b106287e3b91f42b908eb43
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-15 13:34:12 +0530

    Type Casting

---
+++ p2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter value of a and b:" << endl;
+    cin >> a >> b;
+    float result = float(a) / b;
+    cout << "The Result is:" << result;
+    return 0;
+}
