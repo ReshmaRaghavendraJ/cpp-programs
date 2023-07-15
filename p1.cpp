3fe4ea508d0c011c338827b2623cb4e34dffe15e
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-15 13:20:32 +0530

    addition of two numbers

---
+++ p1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Enter the value of a and b:" << endl;
+    cin >> a >> b;
+    c = a + b;
+    cout << "The addition of two number is:" << c;
+    return 0;
+}
