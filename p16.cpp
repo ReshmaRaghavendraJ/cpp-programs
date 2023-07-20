c375356ce3112b9384d95c1b459d5c82b450d1d1
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-20 14:59:37 +0530

    Find array total byte size using sizeof operator

---
+++ p16.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Size n:" << endl;
+    cin >> n;
+    int a[n];
+    cout << "Total size of array is:" << sizeof(a);
+    return 0;
+}
