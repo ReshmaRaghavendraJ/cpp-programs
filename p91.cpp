b0d88c6b386fb99fa281f4001dfc78ea2ce47696
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-28 11:32:59 +0530

    Simple String Class

---
+++ p91.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string address;
+    getline(cin, address);
+    cout << "Your staying at:" << address;
+    return 0;
+}
