98e96373d5fd5a80fa14a199cc8c5f2fd9f7d002
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-28 11:33:49 +0530

    String Length->s.length()

---
+++ p92.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    s = "Hello Welcome to Mantralayam";
+    cout << s.length();
+    return 0;
+}
