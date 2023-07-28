3acce6131214497a9443031f6467ebd5f0ba2d4a
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-28 11:42:21 +0530

    insert()

---
+++ p100.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s = "ComputerScience";
+    s.insert(8, "-");
+    cout << s;
+    return 0;
+}
