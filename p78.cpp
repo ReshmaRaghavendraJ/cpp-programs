7c4c0ad38b8dd459b547bc46d1c7d380553b7e3e
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-28 11:15:53 +0530

    Without Virtual Keyword in pointer

---
+++ p78.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    void camera()
+    {
+        cout << "Basic Camera" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void camera()
+    {
+        cout << "Advanced Feature" << endl;
+    }
+};
+int main()
+{
+    A *ptr;
+    ptr = new B;
+    ptr->camera();
+    ptr = new A;
+    ptr->camera();
+    return 0;
+}
